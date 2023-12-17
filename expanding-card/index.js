7252d31e9d43a489d8d3b11b3a69de7a1f230d98
Abdullah Sharifi <ab.sharifi19@gmail.com>
2023-12-17 23:03:13 +0100

    create index.html, style.css and index.js from zero in expanding-card forlder

---
+++ expanding-card/index.js
@@ -0,0 +1,24 @@
+const panels = document.querySelectorAll('.panel');
+const headings = document.querySelectorAll('h2');
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removePanelOpenClass();
+        panel.classList.add('panel-open');
+    });
+});
+
+
+// function removePanelOpenClass () {
+//     for(panel of panels) {
+//         panel.classList.remove('panel-open')
+//     }
+// };
+
+
+// You can also do it this way:
+function removePanelOpenClass () {
+    panels.forEach((panel) => {
+        panel.classList.remove('panel-open');
+    });
+};
